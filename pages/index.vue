3d413dcdcac5e91ab1f6160fb73ee67003068073
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-07-07 22:00:21 +0900

    no feedback image when dragging

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <div class="row">
     <div class="col-6">
       <p><img @dragstart="dStart" @drag="dMove" @dragend="dEnd" class="drag-and-drop" src="/sea.png"></p>
-      <span id="dummy"></span>
     </div>
     <div class="col-6">
     </div>
@@ -27,15 +26,14 @@ export default {
   },
   methods: {
     dStart () {
-      let el = document.getElementById( "dummy" )
-      event.dataTransfer.setDragImage( el, 0, 0 )
+      var dragImage = document.createElement('img');
+      dragImage.src = '/DragImage.png';
+      event.dataTransfer.setDragImage( dragImage, 0, 0 )
       event.srcElement.style.position = 'absolute'
       event.srcElement.style.zIndex = 1000
       event.srcElement.style.left = event.pageX - event.srcElement.offsetWidth / 2 + 'px'
       event.srcElement.style.top = event.pageY - event.srcElement.offsetHeight / 2 + 'px'
       this.is_dragging = true
-      console.log("DOWN el:" + event.srcElement + "X:" + event.pageX + " Y:" + event.pageY + " OffsetLeft:" + event.srcElement.offsetWidth + " OffsetRight:" + event.srcElement.offsetHeight)
-      console.log(event.dataTransfer)
     },
     dMove () {
       if (this.is_dragging && event.pageX !== 0 && event.pageY !== 0) {
@@ -46,7 +44,6 @@ export default {
     dEnd () {
       if (this.is_dragging) {
         this.is_dragging = false
-        console.log("UP X:" + event.pageX + " Y:" + event.pageY)
       }
     }
   }
