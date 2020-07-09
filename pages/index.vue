27d5d5b2a387596d0884e47d7029eeab4d64c6c0
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-07-09 21:47:55 +0900

    drag and drop svg

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
       <p><img @dragstart="dStart" @drag="dMove" @dragend="dEnd" class="drag-and-drop" src="/sea.png"></p>
     </div>
     <div class="col-6">
-      <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300" style="background-color: #ccc">
-        <circle class="drag-and-drop" cx=100 cy=50 r=30 @mousedown="mDown" @mousemove="dMove" @mouseup="dEnd"/>
+      <svg xmlns="http://www.w3.org/2000/svg" style="background-color:#ccc" width="400" height="300" viewbox="0 0 400 300">
+        <circle class="drag-and-drop" cx=30 cy=30 r=30 fill="blue" @mousedown="mDown" @mousemove="mMove" @mouseup="dEnd"/>
       </svg>
     </div>
   </div>
@@ -21,6 +21,9 @@ export default {
     return {
       x: 0,
       y: 0,
+      cx: 0,
+      cy: 0,
+      r: 0,
       is_dragging: false
     }
   },
@@ -32,31 +35,41 @@ export default {
       var dragImage = document.createElement('img');
       dragImage.src = '/DragImage.png';
       event.dataTransfer.setDragImage( dragImage, 0, 0 )
-      event.srcElement.style.position = 'absolute'
-      event.srcElement.style.zIndex = 1000
-      event.srcElement.style.left = event.pageX - event.srcElement.offsetWidth / 2 + 'px'
-      event.srcElement.style.top = event.pageY - event.srcElement.offsetHeight / 2 + 'px'
+      event.target.style.position = 'absolute'
+      event.target.style.zIndex = 1000
+      event.target.style.left = event.pageX - event.target.offsetWidth / 2 + 'px'
+      event.target.style.top = event.pageY - event.target.offsetHeight / 2 + 'px'
       this.is_dragging = true
-      console.log("Start!")
     },
     dMove () {
       if (this.is_dragging && event.pageX !== 0 && event.pageY !== 0) {
-        event.srcElement.style.left = event.pageX - event.srcElement.offsetWidth / 2 + 'px'
-        event.srcElement.style.top = event.pageY - event.srcElement.offsetHeight / 2 + 'px'
+        event.target.style.left = event.pageX - event.target.offsetWidth / 2 + 'px'
+        event.target.style.top = event.pageY - event.target.offsetHeight / 2 + 'px'
       }
     },
     dEnd () {
       if (this.is_dragging) {
         this.is_dragging = false
+        this.x = 0
+        this.y = 0
+        this.cx = 0
+        this.cy = 0
+        this.r = 0
       }
     },
     mDown () {
-      event.srcElement.style.position = 'absolute'
-      event.srcElement.style.zIndex = 1000
-      event.srcElement.style.left = event.pageX - event.srcElement.offsetWidth / 2 + 'px'
-      event.srcElement.style.top = event.pageY - event.srcElement.offsetHeight / 2 + 'px'
+      this.x = event.pageX
+      this.y = event.pageY
+      this.cx = parseInt(event.target.attributes.cx.value)
+      this.cy = parseInt(event.target.attributes.cy.value)
+      this.r = parseInt(event.target.attributes.r.value) 
       this.is_dragging = true
-      console.log("Start!")
+    },
+    mMove () {
+      if (this.is_dragging && event.pageX !== 0 && event.pageY !== 0) {
+        event.target.setAttribute('cx', event.pageX - this.x + this.cx)
+        event.target.setAttribute('cy', event.pageY - this.y + this.cy)
+      }
     }
   }
 };
